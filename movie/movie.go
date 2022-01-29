cf703e749f1d3503e181b20839d0663385c0975a
David Leeson <djl@carvel.co.uk>
2022-01-29 21:34:06 +0000

    More movies in test data

--- movie/movie.go
+++ movie/movie.go
@@ -10,6 +10,8 @@ func FindAll() ([]Movie, error) {
 		{ID: 1, Name: "Avengers"},
 		{ID: 2, Name: "Ant-Man"},
 		{ID: 3, Name: "Thor"},
+		{ID: 4, Name: "Captain America"},
+		{ID: 5, Name: "A Dogs Life"},
 	}
 	return testData, nil
 }
